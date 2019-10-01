d7147227b08d9db50183adee3973bfe5a6b5827d
Sweep <jeonsh1991@gmail.com>
2019-10-01 21:31:19 +0900

    FORM :: Validation -2.4강

--- front/components/PostForm.vue
+++ front/components/PostForm.vue
@@ -6,6 +6,7 @@
                     outlined
                     auto-grow
                     clearable
+                    v-model="content"
                     label="어떤 신기한 일이 있었나요?"
                     :hide-details="hideDetails"
                     :success-messages="successMessages"
@@ -15,28 +16,55 @@
                 />
                 
             </v-form>
+            <v-btn color="green" type="submit" absolute right>짹짹</v-btn>
+            <v-btn>이미지 업로드</v-btn>
         </v-container>
     </v-card>
 </template>
 
 <script>
+import {mapState } from "vuex";
+
 export default {
     data() {
         return{
-            hideDetails: false,
+            hideDetails: true,
             successMessages: '',
-            success: false
+            success: false,
+            content: '',
         }
+    },
+    computed: {
+        ...mapState(["users/me"])
+        // ...mapState("users", ['me']) 도 가능
     },
     methods: {
         onChangeTextarea() {
-
+            this.hideDetails = true;
+            this.success = false;
+            this.successMessages = '';
         },
         onsubmitForm() {
             if(this.$refs.form.validate()) {
                 this.$store.dispatch("posts/add", {
-                    
+                    content: this.content,
+                    Users: {
+                        nickname: this.me.nickname,
+
+                    },
+                    Comments: [],
+                    Images: [],
+                    id: Date.now(),
+                    createdAt: Date.now(),
                 })
+                .then(() => {
+                    this.hideDetails = false;
+                    this.success = true;
+                    this.successMessages = "게시글 등록 성공!";
+                })
+                .catch( () => {
+
+                });
             }
         }
     }
--- front/store/posts.js
+++ front/store/posts.js
@@ -1,11 +1,19 @@
 export const state = () => ({
-    name: "posts",
+    mainPosts: [],
 });
 // state는 함수여야 한다.
 
 export const mutations = {
-    BYE(state) {
-        state.name = "good posts";
+    addMainPost(state, payload) {
+        state.mainPosts.unshift(payload);
     }
 };
 // mutations 는 함수가 아니어야 한다.
+
+export const actions = {
+    add({commit}, payload) {
+        // 서버에 게시글 등록 요청 보냄.
+        commit("addMainPost");
+        // commit("posts/addMainPost") 해당모듈일 경우 경로는 생략가능
+    }
+}
